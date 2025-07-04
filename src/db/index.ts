aa5dbcb36c0ed8ab760e8e426ecdc42fbda7ad5f
Mihai Adrian Andrei <mihai.andrei1597@gmail.com>
2025-07-04 12:14:29 +0300

    feat: add database configuration

---
+++ src/db/index.ts
@@ -0,0 +1,20 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import * as schema from "./schema";
+import { config } from "dotenv";
+import { expand } from "dotenv-expand";
+
+expand(config());
+
+if( !process.env.DATABASE_URL ) {
+    throw new Error("DATABASE_URL environment variable is not set");
+}
+
+export const client = postgres(process.env.DATABASE_URL, {
+  max: process.env.DB_MIGRATING ? 1 : undefined,
+});
+const db = drizzle(client, {
+  casing: "snake_case",
+  schema,
+});
+export default db;
