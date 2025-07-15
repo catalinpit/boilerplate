6b4913378358ffac2dc204135cf06ecb9eee5104
Catalin Pit <catalinpit@gmail.com>
2025-07-15 18:01:46 +0300

    chore: small visual updates

--- src/app/demo-trpc-protected/page.tsx
+++ src/app/demo-trpc-protected/page.tsx
@@ -4,12 +4,15 @@ import { trpc } from "@/trpc/client";
 
 export default function DemoTrpcProtected() {
   const greeting = trpc.protected.useQuery({ text: "world" });
+
   if (greeting.isLoading) {
     return <div>Loading...</div>;
   }
+
   if (greeting.isError) {
     return <div>Error: {greeting.error.message}</div>;
   }
+
   return (
     <div>
       <h1>Demo TRPC Protected</h1>
